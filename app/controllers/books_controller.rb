847fc3234a4f6838a4634f883140c4b71b5659b5
HiroakiBaba <141119965+Hiroaki027@users.noreply.github.com>
2023-08-17 12:19:52 +0000

    Commit4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,27 @@
 class BooksController < ApplicationController
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(user_id)
+    redirect_to book_path(@book.id)
   end
-  
+
   def index
+    @book = Book.new
     @books = Book.all
-    @users = User.find(params[:id])
+    @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
   end
 
- 
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-end 
+end
