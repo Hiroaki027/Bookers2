a1321237d2e94eaf25b0d6e762e08d07da351d31
HiroakiBaba <141119965+Hiroaki027@users.noreply.github.com>
2023-08-17 13:28:09 +0000

    Commit5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,22 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = current_user
   end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id) 
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
 
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -10,11 +10,17 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    @user.update
-    redirect_to user_path(user)
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
   
   def index
     @users = User.all
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
